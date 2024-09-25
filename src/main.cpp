f8811f228fe1e9a67845fdc22a8494ab6b58747c
Jerome Parent <jejmule@gmail.com>
2024-09-25 15:09:32 +0200

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -4,49 +4,79 @@
 #include <Wire.h>
 #include "UNIT_EXT_ENCODER.h"
 #include "TCA9548.h"
+#include <RunningMedian.h>
+#include "ewma.h"
 
 //modbus communication
-unsigned long baudrate = 57600UL;
+unsigned long baudrate = 115200UL;  //19200, 38400, 57600, 115200
 #define RX 33
 #define TX 23
-ModbusRTUMaster master(Serial1);
+ModbusRTUMaster master(Serial2);
 #define VICON_ADDRESS 4    //modbus address of the Vicon
 
 //register to write on Vicon
 #define REGISTERS_TO_WRITE_VICON 3   //number of register to write : lift angle, belt speed
 uint16_t outputRegistersVicon[REGISTERS_TO_WRITE_VICON] = {0, 0, 0}; //belt speed, lift angle, blet torque
 
+//i2c wire communication
+#define I2C_FREQ 400000UL
+
 //i2c ext-encoder communication
 UNIT_EXT_ENCODER encoder;
+RunningMedian encoder_count_median = RunningMedian(5);
+Ewma encoder_count_ewma = Ewma(0.1);
+Ewma encoder_speed_ewma = Ewma(0.5);
 float compute_encoder_speed();
+float compute_encoder_speed_2(float encoder_count,uint32_t time_us);
+float compute_encoder_speed_3(uint32_t encoder_count);
+//define timer to measure speed
+//https://deepbluembedded.com/esp32-timers-timer-interrupt-tutorial-arduino-ide/?utm_content=cmp-true
+hw_timer_t *timer = NULL;
 
 //i2c multiplexer communication
 TCA9548 i2cMultiplexer(0x70);
 #define ADC_ADDR 0x48
 
-static uint16_t readADC(uint8_t channel, TCA9548 *multiplexer) {
+uint16_t readADC(uint8_t channel, TCA9548 *multiplexer) {
     multiplexer->selectChannel(channel);
     Wire.requestFrom(ADC_ADDR, (uint8_t)2);
     return (int16_t)((Wire.read() << 8) | Wire.read());
 }
 
+uint32_t readEncoder() {
+    Wire.beginTransmission(0x59);
+    Wire.write(0x00); // register 0
+    Wire.endTransmission(true);
+    Wire.requestFrom(UNIT_EXT_ENCODER_ADDR, (uint8_t)4);
+    return (int32_t)((Wire.read() << 24) | (Wire.read() << 16) | (Wire.read() << 8) | Wire.read());
+} 
+
 void setup() {
   //initialize the M5Atom
   M5.begin(true, true, true); // enable serial, enable I2C, enable display (led)
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("M5Atom initialized");
 
   //initialize the modbus communication
-  Serial1.begin(baudrate, SERIAL_8E1, RX, TX);
+  Serial2.begin(baudrate, SERIAL_8E1, RX, TX);
   master.begin(baudrate);
 
+  //initialize the timer
+  //timer = timerBegin(2, 1, true); //timer 0, prescaler 80, count up
+  //timerStart(timer);
+
   //initialize the i2c communication
-  Wire.begin();
-  Wire.setClock(400000);
+  //Wire.setPins(25U, 21U); //default pins (21, 22)
+  Wire.setClock(I2C_FREQ);
+  //Wire.setTimeOut(1000); //1ms timeout
+  Wire.begin(25U, 21U);
+
 
   //initialize the encoder
-  if(encoder.begin(&Wire, UNIT_EXT_ENCODER_ADDR, SDA, SCL, 400000UL)){ //400kHz
+  if(encoder.begin(&Wire, UNIT_EXT_ENCODER_ADDR, 25U, 21U, I2C_FREQ)){
     Serial.println("Encoder initialized");
+    encoder.setPulse(360);
+    encoder.resetEncoder();
   }
   else{
     Serial.println("Encoder initialization failed");
@@ -75,47 +105,88 @@ void setup() {
       Serial.println("ADC device not found on channel " + String(i));
     }
   }
+  
   M5.dis.drawpix(0, CRGB::Green);
 }
 
 bool debug = false;
 
+float encoder_count;
+float encoder_count_filtered;
+float encoder_speed;
+float encoder_speed_filtered;
+
 void loop() {
   //M5 update for button
   M5.update();
+  
   //change parameters with buttons
   if(M5.Btn.wasPressed()){
     debug = !debug;
     if(debug){
         M5.dis.drawpix(0, CRGB::Orange);
         Serial.println("Debug mode enabled");
+        encoder.resetEncoder();
     }
     else{
         M5.dis.drawpix(0, CRGB::Green);
     }
   }
 
+  //read encoder value
+  encoder_count_median.add(encoder.getEncoderValue());
+  static float encoder_cont_last_value = 0;
+  encoder_count = encoder_count_median.getMedian();
+  encoder_count_filtered = encoder_count_ewma.filter(encoder_count);
+  /*float delta_count = encoder_count - encoder_cont_last_value;
+  encoder_cont_last_value = encoder_count;
+
+  //compute encoder speed every 3ms
+  static uint32_t last_time = 0;
+  uint32_t time = micros();
+  uint32_t delta_time = time - last_time;
+  if(time - last_time >= 3000) {
+    encoder_speed = compute_encoder_speed_2(delta_count,delta_time);
+    last_time = time;
+  }*/
+
   //Send Modbus request
   static uint32_t last_request = 0;
-  if (master.isIdle() && ((millis() - last_request) > 1000)) {
+  if (master.isIdle()) {
     const float  max_pulse_sec = 2900.0 / 60 * 1504; //2900 tr/min at 100Hz with 1504 pulse per rotation
     const uint16_t max_voltage = 10000; //10V
+    //compute encoder speed
+    encoder_speed = compute_encoder_speed_2(encoder_count_filtered, micros());
+    //filter encoder speed
+    encoder_speed_filtered = encoder_speed_ewma.filter(encoder_speed);
     //scale speed to 0-10V
-    //outputRegistersVicon[0] = uint16_t(round(max_voltage * compute_encoder_speed() / max_pulse_sec));
+    outputRegistersVicon[0] = uint16_t(round(max_voltage * encoder_speed / max_pulse_sec));
     //scale adc on channel 0 to 0-10'000 mV  
-    //outputRegistersVicon[1] = uint16_t(1.0 * readADC(0, &i2cMultiplexer) * 10000 / pow(2, 14));
+    outputRegistersVicon[1] = uint16_t(1.0 * readADC(0, &i2cMultiplexer) * 10000 / pow(2, 14));
     //scale adc on channel 1 to 0-10'000 mV
-    //outputRegistersVicon[2] = uint16_t(1.0 * readADC(1, &i2cMultiplexer) * 10000 / pow(2, 14));
+    outputRegistersVicon[2] = uint16_t(1.0 * readADC(1, &i2cMultiplexer) * 10000 / pow(2, 14));
 
     //sinus test if test enabled on last modbus register
-    if(debug){
-      const int f = 10; //Hz
+    if(true){
+      const float f = 10; //Hz
       const float omega = 2 * PI * f;
       outputRegistersVicon[2] = sin(omega * micros() * 1e-6) * 1000;
     }
     //measure time between two modbus request
     if(debug) {
-      Serial.println("time between modbus request : " + String(millis() - last_request) + " ms"); //time between two modbus request
+      Serial.println(">encoder_count:"+String(encoder_count)+"|np");
+      Serial.println(">encoder_count_filtered:"+String(encoder_count_filtered)+"|np");
+      //Serial.println(">encoder_pulse:"+String(encoder.getPulse())+"|np");
+      //Serial.println(">encoder_count:"+String(readEncoder())+"|np");
+      Serial.println(">encoder_speed:"+String(encoder_speed)+"|np");
+      Serial.println(">encoder_speed_filtered:"+String(encoder_speed_filtered)+"|np");
+      Serial.println(">adc0:"+String(readADC(0, &i2cMultiplexer))+"|np");
+      Serial.println(">adc1:"+String(readADC(1, &i2cMultiplexer))+"|np");
+    }
+
+    //print last modbus request time every second
+    if(millis() % 100 == 0){
+      Serial.println("> modbus request delai [ms] : " + String((micros() - last_request)*1e-3) + "|np"); //time between two modbus request
     }
    
     //send write request
@@ -124,8 +195,11 @@ void loop() {
       M5.dis.drawpix(0, CRGB::Red);
     }
 
-    last_request = millis();
+    last_request = micros();
   }
+
+  //add 1ms delay to avoid flooding the modbus
+  //delay(1);
 
    //check for modbus response
   if (master.isWaitingResponse() ) {
@@ -141,11 +215,58 @@ void loop() {
   }
 
   //check for exception on master modbus
+  static uint32_t last_exception = 0;
   if (master.hasException()) {
     Serial.println("MODBUS exception: " + String(master.getExceptionMessage()));
     M5.dis.drawpix(0, CRGB::Red);
     master.clearException();
+    last_exception = millis();
   }
+  //switch back led after 10s without exception
+  if (millis() - last_exception > 1000) {
+    if(!debug) M5.dis.drawpix(0, CRGB::Green);
+    else M5.dis.drawpix(0, CRGB::Orange);
+  }
+}
+/*
+float compute_encoder_speed_3(uint32_t encoder_count) {
+  //define last values 
+  static uint64_t last_time = 0;
+  static long last_encoder_count = 0;
+  //compute deltas
+  //read encoder value
+  uint64_t time = timerReadMicros(timer);
+  //compute deltas count and time
+  uint32_t delta_c = encoder_count - last_encoder_count;
+  uint32_t delta_t = time - last_time;
+  Serial.println("delta_c: " + String(delta_c) + " delta_t: " + String(delta_t));
+  //compute speed
+  float encoder_speed = 1e6 * delta_c / delta_t; //impulsions per second
+  Serial.println("encoder_speed: " + String(encoder_speed));
+  //update last values
+  last_encoder_count = encoder_count;
+  last_time = timerReadMicros(timer);
+  //retunr speed pulses per second
+  return encoder_speed;
+}*/
+
+float compute_encoder_speed_2(float encoder_count,uint32_t time_us) {
+  //define last values 
+  static uint32_t last_time_us = 0;
+  static long last_encoder_count = 0;
+  //compute deltas
+  //compute deltas count and time
+  int delta_c = encoder_count - last_encoder_count;
+  uint32_t delta_t = time_us - last_time_us;
+  Serial.println("delta_c: " + String(delta_c) + " delta_t: " + String(delta_t));
+  //compute speed
+  float encoder_speed = 1e6 * delta_c / delta_t; //impulsions per second
+  Serial.println("encoder_speed: " + String(encoder_speed));
+  //update last values
+  last_encoder_count = encoder_count;
+  last_time_us = time_us;
+  //retunr speed pulses per second
+  return encoder_speed;
 }
 
 float compute_encoder_speed() {
@@ -154,13 +275,15 @@ float compute_encoder_speed() {
   static long last_encoder_count = 0;
   //compute deltas
   //read encoder value
-  float encoder_count = encoder.getEncoderValue();
+  uint32_t encoder_count = encoder.getEncoderValue();
   uint32_t time_us = micros();
   //compute deltas count and time
-  uint32_t delta_c = encoder_count - last_encoder_count;
+  int delta_c = encoder_count - last_encoder_count;
   uint32_t delta_t = time_us - last_time_us;
+  Serial.println("delta_c: " + String(delta_c) + " delta_t: " + String(delta_t));
   //compute speed
-  float encoder_speed = delta_c * 1e6 / delta_t; //impulsions per second
+  float encoder_speed = 1e6 * delta_c / delta_t; //impulsions per second
+  Serial.println("encoder_speed: " + String(encoder_speed));
   //update last values
   last_encoder_count = encoder_count;
   last_time_us = time_us;
