cec852fda4abbe06bf68271a2ab99fa943a83eae
Jérôme Parent <jejmule@gmail.com>
2026-08-28 08:01:58 +0200

    clean unused function

--- src/main.cpp
+++ src/main.cpp
@@ -68,15 +68,6 @@ ModbusRTUSlave slave(Serial2,3); //modbus slave on serial2, addr to be checked
 bool coils[NUM_COILS] = {false,false};  //first coil is for steps status and second if for encoder_feedback status
 uint16_t registers[NUM_REGISTERS] = {0}; //belt ot steps encoder speed in mm/s
 
-float readADC() {
-  int length = 2;
-  Wire.requestFrom(ADC_ADDR, length);
-  int16_t raw = (Wire.read() << 8) | Wire.read() ;
-  int16_t min_code = 8192; //14bit 60fps
-  float deltaV = raw * 2.048 / min_code; //conversion ADS1110 in datasheet
-  return deltaV * 6.1; //m5stack ADCv1.1 has a tension diviser made by 510k and 100k resitor (510+100) / 100
-}
-
 uint32_t readEncoder(uint8_t channel, TCA9548 *multiplexer,bool mm=false) {
   if (channel >= max_channel) {
     Serial.println("No encoder on requested channel : " + String(channel));
@@ -134,6 +125,7 @@ void setup() {
   slave.setHoldingRegisters(registers,1); //set holding register to store current feedback speed
   
   delay(1000); //avoid initialisation errors
+
   //I2C communication
   //Wire.begin(SDA, SCL);
   Wire.setClock(I2C_FREQ);  //update clock
@@ -154,11 +146,7 @@ void setup() {
     
     if(connected){
       Serial.println("Encoder device found on channel " + String(i));
-      //send configuration to encoder
       encoders[i].begin(&Wire, ENC_ADDR, SDA, SCL, I2C_FREQ);
-      //encoders[i].setPulse(pulse[i]);
-      //encoders[i].setPerimeter(perimeter[i]);
-      //encoders[i].resetEncoder();
       Serial.println("Encoder initialized");
     }
     else{
@@ -169,41 +157,16 @@ void setup() {
     }
   }
 
-  //initialize ADC device
-
-  //test if ADC is connected
-  Wire.beginTransmission(ADC_ADDR);
-  if (Wire.endTransmission()== 0) {
-    Serial.println("ADC device found");
-    //send configuration to ADC
-    const uint8_t config = 0x84; // 1000 0100 see ADC1110 datasheet : 60fps continuous mode
-    Wire.beginTransmission(ADC_ADDR);
-    Wire.write(0x84); // config register
-    Wire.endTransmission();
-    Serial.println("ADC initialized");
-    }
-  else {
-    Serial.println("ADC device not found");
-    i2c_error += 1;
-  }
-
   //initialize DAC devices
-  i2cMultiplexer.selectChannel(2);
-  if(i2cMultiplexer.isConnected(DAC_ADDR)){
-    Serial.println("DAC device found on channel 2");
-  }
-  else{
-    Serial.println("DAC device not found on channel 2");
-    i2c_error += 1;
-  }
-
-  i2cMultiplexer.selectChannel(3);
-  if(i2cMultiplexer.isConnected(DAC_ADDR)){
-    Serial.println("DAC device found on channel 3");
-  }
-  else{
-    Serial.println("DAC device not found on channel 3");
-    i2c_error += 1;
+  for(int i = 2; i < 4; i++){
+    i2cMultiplexer.selectChannel(i);
+    if(i2cMultiplexer.isConnected(DAC_ADDR)){
+      Serial.println("DAC device found on channel "+String(i));
+    }
+    else{
+      Serial.println("DAC device not found on channel "+String(i));
+      i2c_error += 1;
+    }
   }
 
   //stop execution if there is i2c error
@@ -256,7 +219,7 @@ void loop() {
     last_steps_encoder_count = steps_encoder_count;
     last_time_us = time_us;
     //read adc value
-    inclinaison_median.add(readADC());
+    //inclinaison_median.add(readADC());
     uint32_t inclinaison= round(inclinaison_median.getMedian()*100);  
 
     //apply geometry to compute speed in mm/s
@@ -358,84 +321,6 @@ void loop() {
         M5.dis.drawpix(0, CRGB::Green);
     }
   }
-
-  /*//Send Modbus request
-  static uint32_t last_request = 0;
-  if (master.isIdle()) {
-    const float  max_pulse_sec = 2900.0 / 60 * 1504; //2900 tr/min at 100Hz with 1504 pulse per rotation
-    const uint16_t max_voltage = 10000; //10V
-    //compute encoder speed
-    encoder_speed = compute_encoder_speed_2(encoder_count_filtered, micros());
-    //filter encoder speed
-    encoder_speed_filtered = encoder_speed_ewma.filter(encoder_speed);
-    //scale speed to 0-10V
-    outputRegistersVicon[0] = uint16_t(round(max_voltage * encoder_speed / max_pulse_sec));
-    //scale adc on channel 0 to 0-10'000 mV  
-    outputRegistersVicon[1] = uint16_t(1.0 * readADC(0, &i2cMultiplexer) * 10000 / pow(2, 14));
-    //scale adc on channel 1 to 0-10'000 mV
-    outputRegistersVicon[2] = uint16_t(1.0 * readADC(1, &i2cMultiplexer) * 10000 / pow(2, 14));
-
-    //sinus test if test enabled on last modbus register
-    if(true){
-      const float f = 10; //Hz
-      const float omega = 2 * PI * f;
-      outputRegistersVicon[2] = sin(omega * micros() * 1e-6) * 1000;
-    }
-    //measure time between two modbus request
-    if(debug) {
-      Serial.println(">encoder_count:"+String(encoder_count)+"|np");
-      Serial.println(">encoder_count_filtered:"+String(encoder_count_filtered)+"|np");
-      //Serial.println(">encoder_pulse:"+String(encoder.getPulse())+"|np");
-      //Serial.println(">encoder_count:"+String(readEncoder())+"|np");
-      Serial.println(">encoder_speed:"+String(encoder_speed)+"|np");
-      Serial.println(">encoder_speed_filtered:"+String(encoder_speed_filtered)+"|np");
-      Serial.println(">adc0:"+String(readADC(0, &i2cMultiplexer))+"|np");
-      Serial.println(">adc1:"+String(readADC(1, &i2cMultiplexer))+"|np");
-    }
-
-    //print last modbus request time every second
-    if(millis() % 100 == 0){
-      Serial.println("> modbus request delai [ms] : " + String((micros() - last_request)*1e-3) + "|np"); //time between two modbus request
-    }
-   
-    //send write request
-    if (!master.writeMultipleRegisters(VICON_ADDRESS, 0x0004, outputRegistersVicon, REGISTERS_TO_WRITE_VICON)) {
-      Serial.println("Error writing to VICON");
-      M5.dis.drawpix(0, CRGB::Red);
-    }
-
-    last_request = micros();
-  }
-
-  //add 1ms delay to avoid flooding the modbus
-  //delay(1);
-
-   //check for modbus response
-  if (master.isWaitingResponse() ) {
-    ModbusResponse response = master.available();
-    if (response) {
-      //Serial.println("Answer received from slave "+String(response.getSlave())+" : "+String(response.getFC()));
-      if (response.hasError()) {
-        // There is an error. You can get the error code with response.getErrorCode()
-        Serial.println("Modbus error: " + String(response.getErrorCode()));
-        M5.dis.drawpix(0, CRGB::Red);
-      }
-    }
-  }
-
-  //check for exception on master modbus
-  static uint32_t last_exception = 0;
-  if (master.hasException()) {
-    Serial.println("MODBUS exception: " + String(master.getExceptionMessage()));
-    M5.dis.drawpix(0, CRGB::Red);
-    master.clearException();
-    last_exception = millis();
-  }
-  //switch back led after 10s without exception
-  if (millis() - last_exception > 1000) {
-    if(!debug) M5.dis.drawpix(0, CRGB::Green);
-    else M5.dis.drawpix(0, CRGB::Orange);
-  }*/
 }
 
 
